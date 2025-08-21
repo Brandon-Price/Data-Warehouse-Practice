fac7682b59e5f7c3c7c3ca9c3131a50e635a1616
Brandon Price <brandon.samuel.price@gmail.com>
2025-08-21 17:02:46 -0500

    Add timings of execution to output

--- scripts/data_load.sql
+++ scripts/data_load.sql
@@ -1,18 +1,24 @@
 \echo "====================================="
 \echo "Loading Bronze Layer"
 \echo "====================================="
+
+\timing
 \echo ">> Truncating All Tables"
 TRUNCATE TABLE bronze.crm_cust_info, bronze.crm_prd_info, bronze.crm_sales_details, bronze.erp_cust_az12, bronze.erp_loc_a101, bronze.erp_px_cat_g1v2;
+\timing
 
+\timing
 \echo "-------------------------------------"
 \echo "Loading CRM Tables"
 \echo "-------------------------------------"
-
 \COPY bronze.crm_cust_info FROM 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\datasets\source_crm\cust_info.csv' DELIMITER ',' CSV HEADER;
 
 \COPY bronze.crm_prd_info FROM 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\datasets\source_crm\prd_info.csv' DELIMITER ',' CSV HEADER; 
 
 \COPY bronze.crm_sales_details FROM 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\datasets\source_crm\sales_details.csv' DELIMITER ',' CSV HEADER; 
+\timing
+
+\timing
 \echo "-------------------------------------"
 \echo "Loading ERP Tables"
 \echo "-------------------------------------"
@@ -21,3 +27,4 @@ TRUNCATE TABLE bronze.crm_cust_info, bronze.crm_prd_info, bronze.crm_sales_detai
 \COPY bronze.erp_loc_a101 FROM 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\datasets\source_erp\LOC_A101.csv' DELIMITER ',' CSV HEADER;
 
 \COPY bronze.erp_px_cat_g1v2 FROM 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\datasets\source_erp\PX_CAT_G1V2.csv' DELIMITER ',' CSV HEADER;
+\timing
