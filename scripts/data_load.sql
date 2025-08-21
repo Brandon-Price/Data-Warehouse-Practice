86890864496e772f529eded8d76fc50542e87c7a
Brandon Price <brandon.samuel.price@gmail.com>
2025-08-20 23:53:45 -0500

    Add comments

--- scripts/data_load.sql
+++ scripts/data_load.sql
@@ -1,10 +1,21 @@
-TRUNCATE TABLE bronze.crm_cust_info, bronze.crm_prd_info, bronze.crm_sales_details, bronze.erp_cust_az12, bronze.erp_loc_a101, bronze.erp_px_cat_g1v2; 
+\echo "====================================="
+\echo "Loading Bronze Layer"
+\echo "====================================="
+\echo ">> Truncating All Tables"
+TRUNCATE TABLE bronze.crm_cust_info, bronze.crm_prd_info, bronze.crm_sales_details, bronze.erp_cust_az12, bronze.erp_loc_a101, bronze.erp_px_cat_g1v2;
+
+\echo "-------------------------------------"
+\echo "Loading CRM Tables"
+\echo "-------------------------------------"
+
 \COPY bronze.crm_cust_info FROM 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\datasets\source_crm\cust_info.csv' DELIMITER ',' CSV HEADER;
 
 \COPY bronze.crm_prd_info FROM 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\datasets\source_crm\prd_info.csv' DELIMITER ',' CSV HEADER; 
 
 \COPY bronze.crm_sales_details FROM 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\datasets\source_crm\sales_details.csv' DELIMITER ',' CSV HEADER; 
-
+\echo "-------------------------------------"
+\echo "Loading ERP Tables"
+\echo "-------------------------------------"
 \COPY bronze.erp_cust_az12 FROM 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\datasets\source_erp\CUST_AZ12.csv' DELIMITER ',' CSV HEADER;
 
 \COPY bronze.erp_loc_a101 FROM 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\datasets\source_erp\LOC_A101.csv' DELIMITER ',' CSV HEADER;
