6a27b1c9b7f594e87de48d005e9ccc21ef971205
Brandon Price <brandon.samuel.price@gmail.com>
2025-08-21 00:14:35 -0500

    Create bash script for running truncate and populate

---
+++ scripts/data_load.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+source 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\.env'
+export PGPASSWORD=$DB_PASSWORD
+
+psql -U $DB_USER -d $DB_NAME -f "C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\scripts\data_load.sql" 
+
+if [ $? -ne 0 ]; then
+    echo "Error: psql command failed."
+    exit 1
+else
+    echo "success"
+fi
