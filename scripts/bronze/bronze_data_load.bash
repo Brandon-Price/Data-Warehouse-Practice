64fc0740bc585371d26ad0712bfdfe50f3f29c5e
Brandon Price <brandon.samuel.price@gmail.com>
2025-08-21 20:44:28 -0500

    Minor Edit

--- scripts/bronze/bronze_data_load.bash
+++ scripts/bronze/bronze_data_load.bash
@@ -2,7 +2,7 @@
 source 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\.env'
 export PGPASSWORD=$DB_PASSWORD
 
-psql -U $DB_USER -d $DB_NAME -f "C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\scripts\data_load.sql" 
+psql -U $DB_USER -d $DB_NAME -f "C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\scripts\bronze\bronze_data_load.sql" 
 
 if [ $? -ne 0 ]; then
     echo "Error: psql command failed."
