50c1ecb226db47cef189e573bea4e7789730078a
Brandon Price <brandon.samuel.price@gmail.com>
2025-08-23 20:38:26 -0500

    Create Gold Layer Views, and Create Main Bash File

--- scripts/Data_Load.bash
+++ scripts/Data_Load.bash
@@ -4,4 +4,4 @@ bash '.\bronze\bronze_data_load.bash'
 
 bash '.\silver\silver_data_load.bash'
 
-#bash '.\gold\gold_data_load.bash'
+bash '.\gold\gold_data_load.bash'
---
+++ scripts/gold/gold_data_load.bash
@@ -0,0 +1,11 @@
+source 'C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\.env'
+export PGPASSWORD=$DB_PASSWORD
+
+psql -U $DB_USER -d $DB_NAME -f "C:\Users\Brand\Documents\Engineering\Projects\Data Engineering\Data-Warehouse-Practice\scripts\gold\gold_data_load.sql" 
+
+if [ $? -ne 0 ]; then
+    echo "Error: psql command failed."
+    exit 1
+else
+    echo "success"
+fi
